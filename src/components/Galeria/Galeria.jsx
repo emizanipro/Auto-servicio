5e2d5a3db90f7373cde4d476fea2fab521dc648d
Emiliano Zani <142833557+emizanipro@users.noreply.github.com>
2025-02-21 11:45:16 -0300

    Add files via upload

--- src/components/Galeria/Galeria.jsx
+++ src/components/Galeria/Galeria.jsx
@@ -1,24 +1,70 @@
-import React from 'react';
-
-
+import React, { useState } from 'react';
 import './Galeria.css';
 
 function Galeria() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleImageClick = (src) => {
+    setSelectedImage(src);
+  };
+
+  const closeOverlay = () => {
+    setSelectedImage(null);
+  };
+
   return (
     <div className='Galeria-container'>
-        <div className=''>
-            <div className=''>
-                <img src=""></img>
-            </div>
-            <div className=''>
-                <h3>Galeria!</h3>
-            </div>
+      <div className='galeria-title'>
+        <h3>Galería de Imágenes</h3>
+        <img src="./assets/img-2-logo.png" alt="Logo" />
+      </div>
+
+      <div className='cards-galeria'>
+        {[
+          {
+            title: "Tratamiento Cerámico",
+            images: ["./assets/img-13.png", "./assets/img-13.png", "./assets/img-31.jpg", "./assets/img-31.jpg"]
+          },
+          {
+            title: "Daño por granizo y sacabollos",
+            images: ["./assets/img-22.jfif", "./assets/img-22.jfif", "./assets/img-12.png", "./assets/img-12.png"]
+          },
+          {
+            title: "Decoración de Interiores",
+            images: ["./assets/img-7.jfif", "./assets/img-7.jfif", "./assets/img-7.jfif", "./assets/img-7.jfif"]
+          },
+          {
+            title: "Capacitaciones Car Detail",
+            images: ["./assets/img-11.png", "./assets/img-11.png", "./assets/img-10.png", "./assets/img-10.png"]
+          },
+          {
+            title: "¿ Quienes Somos ?",
+            images: ["./assets/img-30.jpg", "./assets/img-30.jpg", "./assets/img-30.jpg", "./assets/img-30.jpg"]
+          }
+        ].map((card, index) => (
+          <div className='cards-muestra' key={index}>
+            <h3>{card.title}</h3>
+            {card.images.map((imgSrc, imgIndex) => (
+              <img 
+                key={imgIndex} 
+                src={imgSrc} 
+                alt={card.title} 
+                onClick={() => handleImageClick(imgSrc)} 
+              />
+            ))}
+          </div>
+        ))}
+      </div>
+
+      {selectedImage && (
+        <div className="overlay-click-img" onClick={closeOverlay}>
+          <div className="overlay-content">
+            <img src={selectedImage} alt="Ampliada" />
+          </div>
         </div>
-
-        
-
+      )}
     </div>
   );
-};
+}
 
 export default Galeria;
